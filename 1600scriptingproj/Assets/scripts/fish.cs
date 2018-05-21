3ef1286b7d286cf66074783a5e2f7a667147ef5b
Aaron Brown <kilonow1275@hotmail.com>
2018-05-21 10:44:23 -0600

    in class add fish object and class

--- 1600scriptingproj/Assets/scripts/fish.cs
+++ 1600scriptingproj/Assets/scripts/fish.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class animal : ScriptableObject {
+public class Fish : Animal {
+
+    public int ScaleCount;
 
 	// Use this for initialization
 	void Start () {
