d0303f1818b5f87093ea91999a1b8c8edad5cc02
Aaron Brown <kilonow1275@hotmail.com>
2018-06-11 11:27:48 -0600

    in class move and jump player behavior

---
+++ 1600scriptingproj/Assets/scripts/MovePlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MovePlayer : MonoBehaviour {
+
+    
+    private CharacterController controller;
+    private Vector3 newPosition;
+    public float Speed =10.0f;
+    public float Gravity = 9.01f;
+    public float JumpSpeed = 10.0f;
+
+
+    // function to characterController
+    void start()
+    {
+        controller = GetComponent<CharacterController>;
+    }
+
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            newPosition.y = JumpSpeed;
+        }
+
+        newPosition.x = Speed;
+        controller.Move(newPosition * Time.deltaTime);
+        newPosition.y -= Gravity;
+        
+    }
+}
