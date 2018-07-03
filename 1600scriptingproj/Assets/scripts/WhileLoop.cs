fbe970a8f88bb2c77c5d3ed60c64434aca348345
Aaron Brown <kilonow1275@hotmail.com>
2018-07-02 21:46:57 -0600

    jul2 home coroutines and while loops

---
+++ 1600scriptingproj/Assets/scripts/WhileLoop.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WhileLoop : MonoBehaviour
+{
+
+	public Text DisplayText;
+
+	public int i = 3;
+
+	// Use this for initialization
+	void Start ()
+	{
+         // 
+		StartCoroutine(Ticker());
+
+	}
+	// type of code routine which enumerates
+	IEnumerator Ticker()
+	{
+		
+		while (i > 0)
+		{
+			DisplayText.text = i.ToString();
+			
+			//holds loop 
+			yield return new WaitForSeconds(1);
+			i--;
+		}
+		DisplayText.text = "Go!";
+		yield return new WaitForSeconds(2);
+		DisplayText.text = "";
+	}
+
+	private void Update()
+	{
+		
+	}
+}
