f7659c22e0bdc3dbf1eea4e8b45b9c661c66f3fb
Aaron Brown <kilonow1275@hotmail.com>
2018-07-30 22:38:07 -0600

    Auto stash before merge of "master" and "origin/master"

---
+++ 1600scriptingproj/Assets/Animation/BounceOnClick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceOnClick : MonoBehaviour
+{
+
+	private Animator anims;
+
+	private void Start()
+	{
+		anims = GetComponent<Animator>();
+	}
+
+	private void OnMouseDown()
+	{
+		anims.SetTrigger("canBounce");
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		anims.SetTrigger("canBounce");
+	}
+
+	public void EndAnims()
+	{
+		GetComponent<MeshRenderer>().enabled = false;
+		GetComponent<SphereCollider>().enabled = false;
+		GetComponent<ParticleSystem>().Emit(50);
+	}
+}
